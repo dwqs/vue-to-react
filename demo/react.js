0dbde93ad6f9543adde80a377231706ba29690ea
dwqs <461147874@qq.com>
2018-05-18 14:28:41 +0800

    refactor: Update demos

--- demo/react.js
+++ demo/react.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 
+// Component Tip: https://github.com/vuejs/babel-plugin-transform-vue-jsx#component-tip
+import Todo from './Todo.js';
+import 'path/to/vue.less';
+import axios from 'axions';
+
 export default class DemoTest extends Component {
     constructor (props) {
         super(props);
+        const now = Date.now();
         this.state = {
-            title: 'vue to react', msg: 'Hello world' 
+            title: 'vue to react', msg: 'Hello world', time: now 
         };
     }
 
@@ -13,6 +19,7 @@ export default class DemoTest extends Component {
             <div>
                 <p>{this.state.title}</p>
                 <p>{this.state.msg}</p>
+                <Todo />
             </div>
         );
     }
--- demo/vue.js
+++ demo/vue.js
@@ -1,9 +1,17 @@
+import axios from 'axions';
+
+import 'path/to/vue.less';
+// Component Tip: https://github.com/vuejs/babel-plugin-transform-vue-jsx#component-tip
+import Todo from './Todo.js';
+
 export default {
     name: 'demo-test',
     data () {
+        const now = Date.now();
         return {
             title: 'vue to react',
-            msg: 'Hello world'
+            msg: 'Hello world',
+            time: now
         }
     },
 
@@ -12,6 +20,7 @@ export default {
             <div>
                 <p>{this.title}</p>
                 <p>{this.msg}</p>
+                <Todo></Todo>
             </div>
         )
     }
